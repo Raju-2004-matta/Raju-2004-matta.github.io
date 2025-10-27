fc95f867b13ec035af6719ef0992dafa2d5b58ab
MATTA RAJU <rajureddymatta03@gmail.com>
2025-10-27 20:43:57 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Set dynamic year in footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Simple greeting in console
+console.log("👋 Welcome to my personal site!");
